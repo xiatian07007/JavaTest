6724eb011167920a146cf22cdeca95a4670a76b7
tianmeimei <tianmeimei@qq.com>
2026-06-10 22:47:25 +0800

    一些小练习

--- src/Practice01.java
+++ src/Practice01.java
@@ -1,5 +1,6 @@
 
 //💡 小技巧：在 main 方法里输入 psvm 然后按 Tab，IDEA 会自动补全整个 main 方法；输入 sout 按 Tab 自动补全 System.out.println()，非常方便！
+//基础运算符
 public class Practice01 {
     public static void main(String[] args) {
 int a=10,b=3;
@@ -18,6 +19,43 @@ int a=10,b=3;
         System.out.println(x&&y);
         System.out.println(x||y);
         System.out.println(!x);
+
+
+        //条件判断
+             int score=85;
+                     if(score>=90){
+                             System.out.println("优秀");
+                     }else if(score>=60){
+                             System.out.println("及格");
+                     }else{
+                             System.out.println("不及格");
+                     }
+
+
+
+//    switch语句
+            int day=3;
+            switch (day){
+                    case 1:System.out.println("周一"); break;
+                    case 2:System.out.println("周一"); break;
+                    case 3:System.out.println("周一"); break;
+                    default:System.out.println("其它");
+            }
+
+
+
+            //数组
+            int[] nums={10,20,30,40,50};
+            //访问元素
+            System.out.println(nums[0]);
+            //遍历数组
+            for(int i=0;i<nums.length;i++){
+                    System.out.println(nums[i]);
+            }
+            //增强for循环
+            for(int n:nums){
+                    System.out.println(n);
+            }
     }
 
 
---
+++ src/TypeNumber.java
@@ -0,0 +1,46 @@
+import com.sun.tools.jconsole.JConsoleContext;
+
+public class TypeNumber {
+    public static void main(String[] args){  //程序入口固定写法
+//判断是奇数还是偶数
+        int listData=100;
+                if(listData%2==0){
+                    System.out.println("我是偶数");
+                }else{
+                    System.out.println("我是奇数");
+                }
+
+
+                //使用for循环
+        int sum=0;
+        for(int i= 1;i<=100;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+
+
+        //找出数组中的最大值
+        int maxlis=0;
+        int[] numberArray={1,9,3,4,5};
+
+        maxlis = numberArray[0];
+        for (int i = 0; i < numberArray.length; i++) {
+
+            if(maxlis<numberArray[i]){
+                maxlis=numberArray[i];
+
+            }
+        }
+        System.out.println(maxlis);
+//打印9*9乘法表
+
+
+        for(int i=1;i<=9;i++){
+         for (int j=1;j<=i;j++){
+             System.out.println(j + "×" + i + "=" + (i*j)+ "\t");
+         }
+            System.out.println();
+        }
+        }
+
+}
