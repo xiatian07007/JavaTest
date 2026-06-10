d3eab9706051cd42536342e5f68da1c68db2e1a6
tianmeimei <tianmeimei@qq.com>
2026-06-10 18:05:07 +0800

    初次提奥

---
+++ src/Helloworld.java
@@ -0,0 +1,29 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+//void main() {
+//    //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+//    // 查看 IntelliJ IDEA 建议如何修正。
+//    IO.println(String.format("Hello and welcome!"));
+//
+//    for (int i = 1; i <= 5; i++) {
+//        //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
+//        // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
+//        IO.println("i = " + i);
+//    }
+//}
+
+public class Helloworld {
+  public static void main(String[] args){  //程序入口固定写法
+      System.out.println("Hello world!");
+  }
+}
+//变量和数据类型
+/**
+ * int age=18; //整数
+ * double proce=9.9; //小数
+ * char letter ='A'; //单个字符串
+ * bollean flag=true; //布尔型
+ * String name="小明"; //字符串
+ */
+
+
---
+++ src/Practice01.java
@@ -0,0 +1,24 @@
+
+//💡 小技巧：在 main 方法里输入 psvm 然后按 Tab，IDEA 会自动补全整个 main 方法；输入 sout 按 Tab 自动补全 System.out.println()，非常方便！
+public class Practice01 {
+    public static void main(String[] args) {
+int a=10,b=3;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b); //取余数
+
+        //比较运算符
+        System.out.println(a>b);
+        System.out.println(a==b);
+
+        //逻辑运算符
+        boolean y=false,x=true;
+        System.out.println(x&&y);
+        System.out.println(x||y);
+        System.out.println(!x);
+    }
+
+
+}
